09f233d07ca2f627912d82a18d1abe8628dc04f0
Norse Code <frc7795@gmail.com>
2020-01-20 13:01:53 -0500

    initial checkin :)

---
+++ CommandRobot/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    // CAN IDs
+    //FL == front left, etc.
+    public static final int FL_DRIVE_CAN_ID = 1;
+    public static final int FR_DRIVE_CAN_ID = 2;
+    public static final int RL_DRIVE_CAN_ID = 3;
+    public static final int RR_DRIVE_CAN_ID = 4;
+
+    //Joystick port IDs
+    public static final int CONTROLLER_USB_ID = 0;
+}
---
+++ CommandRobot/src/main/java/frc/robot/commands/ArcadeDriveCommand.java
@@ -0,0 +1,71 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class ArcadeDriveCommand extends CommandBase {
+  DrivetrainSubsystem m_drivetrain;
+  Joystick m_controller;
+  private double lastSetPower = 0.0;
+  private double magicPowerFraction = 0.05;
+  /**
+   * Creates a new TankDriveCommand.
+   */
+  public ArcadeDriveCommand(DrivetrainSubsystem drivetrain, Joystick controller) {
+    m_drivetrain = drivetrain;
+    m_controller = controller;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+ // private static double clip(double val) {
+   // return Math.max(-1.0, Math.min(1.0, val));
+  //}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //Most of this is copied/adapted from last year's code so it may not work
+    m_drivetrain.drive(m_controller.getY(Hand.kLeft), m_controller.getRawAxis(4));
+    double steeringSpeedScale = 0.6;
+    double steering = m_controller.getRawAxis(4);
+    double power = m_controller.getY(Hand.kLeft);
+
+    power = lastSetPower + magicPowerFraction * (power - lastSetPower);
+    lastSetPower = power;
+
+    steering = steering * steeringSpeedScale;
+    //double powerLeft = clip(power - steering);
+    //double powerRight = clip(power + steering);
+
+    Robot.drivetrain.drive(power, steering);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    //Stops the motors
+    m_drivetrain.drive(0.0, 0.0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
