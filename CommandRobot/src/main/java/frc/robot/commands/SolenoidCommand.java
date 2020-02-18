b50ad67d12cb4a65a5ad5a88b901112b6d609961
Terminal <magikoopa6@gmail.com>
2020-02-18 11:02:49 -0500

    Implemented solenoid commands :)

---
+++ CommandRobot/src/main/java/frc/robot/commands/SolenoidCommand.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.subsystems.SolenoidSubsystem;
+
+public class SolenoidCommand extends CommandBase {
+        SolenoidSubsystem m_solenoid;
+        Joystick m_controller;
+        public SolenoidCommand(SolenoidSubsystem solenoid, Joystick controller){
+            m_solenoid = solenoid;
+            m_controller = controller;
+            addRequirements(m_solenoid);
+        }
+
+        @Override
+        public void execute() {
+            boolean noid = m_controller.getRawButtonPressed(Constants.Y);
+
+            SmartDashboard.putBoolean("Solenoid", noid);
+        }
+    }
+  
