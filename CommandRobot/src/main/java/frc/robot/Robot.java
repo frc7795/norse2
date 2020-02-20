ef5f42a3ebf22893f833c0723418ff41d614c2e9
Norse Code <frc7795@gmail.com>
2020-02-20 09:56:25 -0500

    commit

--- CommandRobot/src/main/java/frc/robot/Robot.java
+++ CommandRobot/src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.DrivetrainSubsystem;
@@ -80,6 +81,15 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
+    double time = Timer.getFPGATimestamp();
+
+    if (time < 3) {
+      Robot.drivetrain.drive(0.2, 0.0);
+    }
+
+    else {
+      Robot.drivetrain.drive(0.0, 0.0);
+    }
   }
 
   @Override
